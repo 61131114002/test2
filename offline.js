26f4bd7a1c07b1ea825f5c627d59e8e6e1c3ae77
61131114002 <43134332+61131114002@users.noreply.github.com>
2018-09-10 18:18:56 +0700

    test2

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1536578103,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/bg-sheet0.png",
+		"images/box-sheet0.png",
+		"images/player-sheet0.png",
+		"images/arrowr-sheet0.png",
+		"images/arrowr-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/objct-sheet0.png",
+		"images/tap-sheet0.png",
+		"images/arrowstart-sheet0.png",
+		"media/woosh sound effect (128  kbps) (greenmp3.com).m4a",
+		"media/woosh sound effect (128  kbps) (greenmp3.com).ogg",
+		"media/1.m4a",
+		"media/bg.m4a",
+		"media/bg.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
